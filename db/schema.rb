3c15a652ac4b2eff99685fa8b28c0ac00a52bd48
Jay-Ar Haro <42559012+jgharo@users.noreply.github.com>
2020-03-16 16:22:43 -0500

    removed obsolete files

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_14_014116) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_03_14_014116) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
