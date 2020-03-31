cafd867610921ae1c23ec9900e3e8b83e58c501b
Jay-Ar Haro <42559012+jgharo@users.noreply.github.com>
2020-03-30 19:33:45 -0500

    create table 'stlclients'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_231006) do
+ActiveRecord::Schema.define(version: 2020_03_28_015847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,20 @@ ActiveRecord::Schema.define(version: 2020_03_27_231006) do
     t.index ["prodstatus_id"], name: "index_products_on_prodstatus_id"
   end
 
+  create_table "stlclients", force: :cascade do |t|
+    t.string "client_fname"
+    t.string "client_lname"
+    t.string "client_email"
+    t.string "client_phonenumber"
+    t.string "client_companyname"
+    t.string "client_shippingaddress"
+    t.string "client_billingaddress"
+    t.bigint "employee_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_stlclients_on_employee_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +88,5 @@ ActiveRecord::Schema.define(version: 2020_03_27_231006) do
 
   add_foreign_key "products", "prodcategories"
   add_foreign_key "products", "prodstatuses"
+  add_foreign_key "stlclients", "employees"
 end
