013355a3aceecba412f4c1fc1e250bb5b6f78afd
Anson Dao <ansondao1@gmail.com>
2020-04-06 16:32:11 -0500

    added calculation functionality to service and product orders in invoices

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,4 +1,10 @@
 class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :invoice
+
+  before_save :calculate_product_cost_total
+
+  def calculate_product_cost_total
+    self.product_cost_total = self.product_quantity * self.product.product_cost
+  end
 end
