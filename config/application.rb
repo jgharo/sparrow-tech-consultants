0d70705ba503c26eeccbd8d21500750bb508525c
Jay Haro <jgharo@uh.edu>
2020-04-10 04:00:52 -0500

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Rails.autoloaders.main.ignore(Rails.root.join('app/junkyard'))
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
